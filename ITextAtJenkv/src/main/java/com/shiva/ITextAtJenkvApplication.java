17490698e3d1f338ecb82911ed2296dfdf1daf8d
SHIVA <shiva1607502.office@gmail.com>
2018-09-20 00:03:15 +0530

    Initial ITextExamples

---
+++ ITextAtJenkv/src/main/java/com/shiva/ITextAtJenkvApplication.java
@@ -0,0 +1,12 @@
+package com.shiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITextAtJenkvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITextAtJenkvApplication.class, args);
+	}
+}
